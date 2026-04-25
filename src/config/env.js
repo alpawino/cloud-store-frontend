8eed3a2b3367bdaab73a45ed8ba1cd541118e793
alpawino <jean.aquino@utec.edu.pe>
2026-04-25 14:54:10 -0500

    conexion uwu

--- src/config/env.js
+++ src/config/env.js
@@ -1,13 +1,13 @@
 // src/config/env.js
 // IMPORTANTE: Nunca subas credenciales directamente aquí. Usa variables de entorno (.env).
-// Cuando tus compañeros desplieguen el Backend en AWS API Gateway, cambia esta URL.
+
 
 export const API_CONFIG = {
-  // Actualmente usando mocks locales. Cambiar a true cuando el backend esté listo.
-  USE_MOCKS: true,
-  
-  // URL base del API Gateway (Ej: https://x8y9z0a1b2.execute-api.us-east-1.amazonaws.com/prod)
-  BASE_URL: import.meta.env.VITE_API_GATEWAY_URL || 'http://localhost:8000/api',
+
+  USE_MOCKS: false,
+
+
+  BASE_URL: import.meta.env.VITE_API_GATEWAY_URL || 'http://3.87.171.243:8083/api',
 
   // Endpoints específicos por microservicio (Por si el Gateway enruta por paths)
   ENDPOINTS: {
